45afce2af25f4dbdc1158a4a3238ea046eebf7ad
TELEAGENTBOT2016 <amirhoseinshamlou123456789@gmail.com>
2016-02-27 09:20:23 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users =(152816907،140010873،137791771)--Sudo users
+    sudo_users =(140010873)--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
